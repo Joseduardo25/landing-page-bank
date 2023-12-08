7bb0a9cf20f219990eeb3a1b7d6f031515b14fed
Eduardo Calachua <jocdwardo25@gmail.com>
2023-12-08 02:19:01 -0500

    add FirstBlock and CounterData components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { BestBusiness } from "@/components/BestBusiness";
+import { CounterData } from '@/components/CounterData';
+import { FirstBlock } from '@/components/FirstBlock';
 import { Footer } from "@/components/Footer/";
 import { Header } from "@/components/Header";
 import { PaymentsMethods } from "@/components/PaymentsMethods";
@@ -6,7 +8,9 @@ import { PaymentsMethods } from "@/components/PaymentsMethods";
 export default function Home() {
   return (
     <>
-    <Header></Header>
+      <Header/>
+      <FirstBlock/>
+      <CounterData/>
       <p>From Home Components</p>
     </>
   )
