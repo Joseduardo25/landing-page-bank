b0ff3081cdf65650a708c77715388e226c0b790f
Eduardo Calachua <jocdwardo25@gmail.com>
2023-12-12 15:26:03 -0500

    fix: change export default to export, fix a grammar to class absolute and delete public to prop src to label Image

--- src/components/BackgroundRadialLeft/BackgroundRadialLeft.tsx
+++ src/components/BackgroundRadialLeft/BackgroundRadialLeft.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
 
-const BackgroundRadialLeft = () => {
+export const BackgroundRadialLeft = () => {
   return (
-    <div className=' left-0 absolutea -z-50 top-0'>
-      <Image src='/public/assets/background-floated-left.png' alt='Circle Left' width={850} height={650}/>
+    <div className=' left-0 absolute -z-50 top-0'>
+      <Image src='/assets/background-floated-left.png' alt='Circle Left' width={850} height={650}/>
     </div>
   )
 }
-
-export default BackgroundRadialLeft
