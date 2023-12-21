50200723cddfebd7007b7f153e52c39702cc47c6
Eduardo Calachua <jocdwardo25@gmail.com>
2023-12-20 23:29:59 -0500

    add: Testimonials component and Testimonials data

---
+++ src/components/Testimonials/Testimonials.data.ts
@@ -0,0 +1,43 @@
+export const dataTestimonials = [
+  {
+    id: 1,
+    name: 'Juan Recio',
+    work: 'Founder',
+    testimonials: 'Muy buena landing page SPA sobre como hacer un banco',
+    image: 'profile1'
+  },
+  {
+    id: 2,
+    name: 'Laura García',
+    work: 'Founder',
+    testimonials: 'Muy buena landing page SPA sobre como hacer un banco',
+    image: 'profile2'
+  },
+  {
+    id: 3,
+    name: 'Lucas Martínez',
+    work: 'Founder',
+    testimonials: 'Muy buena landing page SPA sobre como hacer un banco',
+    image: 'profile3'
+  },
+  {
+    id: 4,
+    name: 'Vicente Calvo',
+    work: 'Founder',
+    testimonials: 'Muy buena landing page SPA sobre como hacer un banco',
+    image: 'profile4'
+  },
+  {
+    id: 5,
+    name: 'Patricia Garcia',
+    work: 'Founder',
+    testimonials: 'Muy buena landing page SPA sobre como hacer un banco',
+    image: 'profile1'
+  }, {
+    id: 6,
+    name: 'Raul Vidal',
+    work: 'Founder',
+    testimonials: 'Muy buena landing page SPA sobre como hacer un banco',
+    image: 'profile2'
+  }
+]
---
+++ src/components/Testimonials/Testimonials.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { BackgroundRadialLeft } from '../BackgroundRadialLeft'
+import { Reveal } from '../Reveal'
+import { Slides } from './Slides'
+
+export const Testimonials = () => {
+  return (
+    <div className='relative p-8 md:py-64' id='testimonios'>
+      <BackgroundRadialLeft/>
+      <div className='grid max-w-5xl gap-8 mx-auto my-6 md:grid-cols-2'>
+        <Reveal>
+          <h2 className='text-5xl mb-5 font-semibold'>
+            Qué dicen los clientes de nosotros
+          </h2>
+        </Reveal>
+        <Reveal>
+          <div className='self-center'>
+            <p className='text-primaryDark'>
+              Todo lo que necesitas para aceptar pagos con tarjetas y 
+              hacer crecer tu negocio en cualquier parte del planeta
+            </p>
+          </div>
+        </Reveal>
+      </div>
+      <Slides/>
+    </div>
+  )
+}
