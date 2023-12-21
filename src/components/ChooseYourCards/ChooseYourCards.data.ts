a21e1b3df3917d657960e681f8d095ee7688c73d
Eduardo Calachua <jocdwardo25@gmail.com>
2023-12-20 23:25:57 -0500

    add: ChooseYourCards component and ChooseYourCards data

---
+++ src/components/ChooseYourCards/ChooseYourCards.data.ts
@@ -0,0 +1,14 @@
+export const dataChooseYourCards = [
+  {
+    id: 1,
+    image: 'card-1',
+  },
+  {
+    id: 2,
+    image: 'card-2',
+  },
+  {
+    id: 3,
+    image: 'card-3',
+  },
+]
---
+++ src/components/ChooseYourCards/ChooseYourCards.tsx
@@ -0,0 +1,42 @@
+'use client'
+import 'swiper/css'
+import 'swiper/css/effect-cards'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { BackgroundRadialRight } from '../BackgroundRadialRight'
+import { MotionTransition } from '../MotionTransition'
+import { Reveal } from '../Reveal'
+import { EffectCards } from 'swiper/modules'
+import { dataChooseYourCards } from './ChooseYourCards.data'
+import Image from 'next/image'
+
+export const ChooseYourCards = () => {
+  return (
+    <div className='relative px-6 py-20 md:py-64' id='tarjetas'>
+      <BackgroundRadialRight/>
+      <div className='block max-w-5xl mx-auto md:grid md:grid-cols-2'>
+        <Reveal>
+          <h2 className='text-5xl font-semibold'>Elige la tarjeta que más
+            <span className='block degradedBlue bg-blueLight'>se adapta a tus necesidades</span>
+          </h2>
+        </Reveal>
+        <div className='px-5'>
+          <MotionTransition>
+            <Swiper
+              effect={"cards"}
+              grabCursor={true}
+              modules={[EffectCards]}
+            >
+              {dataChooseYourCards.map(({id,image})=>{
+                return(
+                  <SwiperSlide key={id}>
+                    <Image src={`/assets/${image}.png`} alt='Cards'width={400} height={300}/>
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
+          </MotionTransition>
+        </div>
+      </div>
+    </div>
+  )
+}
