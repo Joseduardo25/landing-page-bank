eeb928dbbb41427c48f510ef893affcf2e063361
Eduardo Calachua <jocdwardo25@gmail.com>
2023-12-20 23:37:29 -0500

    add: new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { BestBusiness } from "@/components/BestBusiness";
+import { ChooseYourCards } from '@/components/ChooseYourCards';
+import { ControlBilling } from '@/components/ControlBilling/ControlBilling';
 import { CounterData } from '@/components/CounterData';
 import { FirstBlock } from '@/components/FirstBlock';
 import { Footer } from "@/components/Footer/";
 import { Header } from "@/components/Header";
 import { PaymentsMethods } from "@/components/PaymentsMethods";
+import { Testimonials } from '@/components/Testimonials';
 
 export default function Home() {
   return (
@@ -11,7 +14,12 @@ export default function Home() {
       <Header/>
       <FirstBlock/>
       <CounterData/>
-      <p>From Home Components</p>
+      <BestBusiness/>
+      <ChooseYourCards/>
+      <ControlBilling/>
+      <Testimonials/>
+      <PaymentsMethods/>
+      <Footer/>
     </>
   )
 }
